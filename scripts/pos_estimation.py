49f916ac220e425ca4a800ddd100dd46d9e9c450
Cesard97 <cd.garrido@uniandes.edu.co>
2019-05-28 18:17:35 +0100

    Fixed pos_estimation node

--- scripts/pos_estimation.py
+++ scripts/pos_estimation.py
@@ -7,9 +7,10 @@ from geometry_msgs.msg import Pose, Point, Twist
 from std_msgs.msg import String, Float32MultiArray
 from robotica_final.srv import *
 from master_msgs_iele3338.msg import Covariance
+from robotica_final.msg import realVel
 
 # Constantes
-L = 100.0
+L = 80.0
 radius = 29.3/2
 
 class posEstimator:
@@ -40,7 +41,7 @@ class posEstimator:
         # Node init
         rospy.init_node('pos_estimation', anonymous=False)
         # Topic subscriber
-        rospy.Subscriber('real_vel', Float32MultiArray, self.real_vel_callback)
+        rospy.Subscriber('real_vel', realVel, self.real_vel_callback)
         # Topic publisher
         pubUncertainty = rospy.Publisher('robot_uncertainty', Covariance, queue_size=10)
         pubPos = rospy.Publisher('robot_position', Pose, queue_size=10)
@@ -52,7 +53,7 @@ class posEstimator:
         # Pos estimation
         prevVelR = 0.0
         prevVelL = 0.0
-        prevTime = 0.0
+        prevTime = 0.1
         actualVelR = 0.0
         actualVelL = 0.0
         actualTime = 0.0
@@ -69,9 +70,10 @@ class posEstimator:
             actualVelR = self.angularVelRight*radius
             actualTime = self.timeStamp
 
+
             # Calculate delta for each wheel
-            deltaSL = (actualVelL - prevVelL)/(actualTime - prevTime)
-            deltaSR = (actualVelR - prevVelR)/(actualTime - prevTime)
+            deltaSL = (actualVelL)*(actualTime - prevTime)
+            deltaSR = (actualVelR)*(actualTime - prevTime)
             # Calculate local delta
             deltaTheta = (deltaSR - deltaSL)/(2*L)
             deltaS = (deltaSL + deltaSR)/2
@@ -86,6 +88,7 @@ class posEstimator:
             self.estimated.position.y = self.actualPos[1]
             self.estimated.orientation.w = self.actualPos[2]
             pubPos.publish(self.estimated)
+
 
             # Update prev variables
             prevVelL = actualVelL
@@ -103,17 +106,18 @@ class posEstimator:
             sigmadS = np.matrix([[0.1*deltaSR, 0.0 ],[0.0, 0.1*deltaSL]])
             # Build Covariance
             self.sigmaP = Fp*self.sigmaP*np.transpose(Fp) + FdS*sigmadS*np.transpose(FdS)
+
             # Build and publish message
-            self.estimatedUncertainty.sigma11 = self.sigmaP[0][0]
-            self.estimatedUncertainty.sigma12 = self.sigmaP[0][1]
-            self.estimatedUncertainty.sigma13 = self.sigmaP[0][2]
-            self.estimatedUncertainty.sigma21 = self.sigmaP[1][0]
-            self.estimatedUncertainty.sigma22 = self.sigmaP[1][1]
-            self.estimatedUncertainty.sigma23 = self.sigmaP[1][2]
-            self.estimatedUncertainty.sigma31 = self.sigmaP[2][0]
-            self.estimatedUncertainty.sigma32 = self.sigmaP[2][1]
-            self.estimatedUncertainty.sigma33 = self.sigmaP[2][2]
-            pubUncertainty.publish(self.estimatedUncertainty)
+            self.estimatedUncertainty.sigma11 = self.sigmaP[0,0]
+            self.estimatedUncertainty.sigma12 = self.sigmaP[0,1]
+            self.estimatedUncertainty.sigma13 = self.sigmaP[0,2]
+            self.estimatedUncertainty.sigma21 = self.sigmaP[1,0]
+            self.estimatedUncertainty.sigma22 = self.sigmaP[1,1]
+            self.estimatedUncertainty.sigma23 = self.sigmaP[1,2]
+            self.estimatedUncertainty.sigma31 = self.sigmaP[2,0]
+            self.estimatedUncertainty.sigma32 = self.sigmaP[2,1]
+            self.estimatedUncertainty.sigma33 = self.sigmaP[2,2]
+            #pubUncertainty.publish(self.estimatedUncertainty)
 
             rate.sleep()
 
