de7aa27720cff38369385a0d61a63a263190caa0
Nicolas Rocha Pacheco <n.rocha11@uniandes.edu.co>
2019-05-28 00:46:50 -0500

    Rhapsody toolit file

---
+++ scripts/rhapsody_toolkit.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+import rospy
+
+
+
+class RhapsodyToolkit():
+	"""docstring for RhapsodyToolkit"""
+	def __init__(self):
+		super(RhapsodyToolkit, self).__init__()
+		self.arg = arg
+
+
+
+
+
