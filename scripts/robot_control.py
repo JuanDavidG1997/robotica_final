c21a53b1fa048de3e6ce5501a16d5ae6d6cc827c
Juan David Garcia Hernandez <44007825+JuanDavidG1997@users.noreply.github.com>
2019-05-29 09:30:23 -0500

    Update robot_control.py

--- scripts/robot_control.py
+++ scripts/robot_control.py
@@ -7,7 +7,7 @@ from std_msgs.msg import Int32
 from robotica_final.srv import *
 
 # Threshold
-THRESHOLD_LIN = 100
+THRESHOLD_LIN = 300
 THRESHOLD_ANG = 0.2
 # States
 WAITING = 0
@@ -16,9 +16,9 @@ EMERGENCY_STOP = 2
 FINISHED = 3
 POSITIONING = 4
 # Control variables
-K_RHO = 0.6
+K_RHO = 0.3
 K_ALPHA = 0.8
-K_BETA = -0.025
+K_BETA = -0.01
 K = [K_RHO, K_ALPHA, K_BETA]
 
 
